6e9937c878ce12d7bd410f6af8ca1cadda83cd54
46494456 <brandon.bui@students.mq.edu.au>
2021-07-28 19:04:53 +1000

    OnMouseDown function updated
    color of circle changes when clicked

--- COMP2160 Prac Week 1/Assets/Scripts/Mole.cs
+++ COMP2160 Prac Week 1/Assets/Scripts/Mole.cs
@@ -6,12 +6,19 @@ using UnityEngine;
 public class Mole : MonoBehaviour
 {
     public Color circleColor;
+    public Color circleColorChange;
 
     // Start is called before the first frame update
     void Start()
     {
         SpriteRenderer sprite = GetComponent<SpriteRenderer>();
         sprite.color = circleColor;
+    }
+
+    void OnMouseDown()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        sprite.color = circleColorChange;
     }
 
     // Update is called once per frame
