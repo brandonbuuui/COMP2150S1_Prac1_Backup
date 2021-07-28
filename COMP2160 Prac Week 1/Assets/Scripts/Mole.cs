30abeaf320b0af63c48e6eab5a531c5d4c3b81db
46494456 <brandon.bui@students.mq.edu.au>
2021-07-28 18:55:23 +1000

    Updated Mole.cs Color
    Finished Step 3

--- COMP2160 Prac Week 1/Assets/Scripts/Mole.cs
+++ COMP2160 Prac Week 1/Assets/Scripts/Mole.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 public class Mole : MonoBehaviour
 {
+    public Color circleColor;
+
     // Start is called before the first frame update
     void Start()
     {
         SpriteRenderer sprite = GetComponent<SpriteRenderer>();
-        sprite.color = Color.red;
+        sprite.color = circleColor;
     }
 
     // Update is called once per frame
