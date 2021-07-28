dfc63298f8f11c18362393d7518ea1f3dc3cfb87
46494456 <brandon.bui@students.mq.edu.au>
2021-07-28 19:30:29 +1000

    Set Color Change Timer in Mole.cs
    colour of circle changes after a certain time once its clicked

--- COMP2160 Prac Week 1/Assets/Scripts/Mole.cs
+++ COMP2160 Prac Week 1/Assets/Scripts/Mole.cs
@@ -5,25 +5,33 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 public class Mole : MonoBehaviour
 {
+    private SpriteRenderer sprite;
     public Color circleColor;
     public Color circleColorChange;
+    public float timer;
+    private float privateTimer;
 
     // Start is called before the first frame update
     void Start()
     {
-        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        sprite = GetComponent<SpriteRenderer>();
         sprite.color = circleColor;
+        privateTimer = timer;
     }
 
     void OnMouseDown()
     {
-        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        privateTimer = timer;
         sprite.color = circleColorChange;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        privateTimer -= Time.deltaTime;
+        if (privateTimer <= 0)
+        {
+            sprite.color = circleColor;
+        }
     }
 }
